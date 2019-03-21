5ef278615c32fd9fa658922b1d38f9756a6e7f91
Dameon Green <dameongreen@gmail.com>
2019-03-20 21:03:54 -0400

    updated

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 
 const Index = () => (
   <Layout title="Home">
-    <p>Welcome to the home page</p>
+    <p>Welcome to the home page222</p>
   </Layout>
 );
 
