f82477806f917ff9f79f5360d38d146a5490614d
Dameon Green <dameongreen@gmail.com>
2019-04-18 10:54:05 -0400

    test

--- index.js
+++ index.js
@@ -1,44 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Saas from './saas';
-import 'normalize.css/normalize.css';
-
-import AddOption from './components/AddOption';
+//import 'normalize.css/normalize.css';
 
 
-
-const Header = (props) => {
-    return (
-        <div>
-            <h1>{props.title}</h1>
-            {props.subtitle && <h2>{props.subtitle}</h2>}
-        </div>
-    );
-};
-
-Header.defaultProps = {
-    title: 'Indecision'
-};
-
-
-
-// Options -> Options component here
-
-
-// Setup options prop for Options component
-
-// addd
-
-
-const User = () => {
-    return (
-        <div>
-            <p> Name: </p>
-            <p>Age: </p>
-        </div>
-    )
-
-}
 
 
 ReactDOM.render(<Saas />, document.getElementById('app'));
